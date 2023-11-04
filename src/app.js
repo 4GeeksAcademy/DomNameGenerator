58c804ccbd2df920dfc6e0efdc54bfcd96f5c85e
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-11-04 00:51:20 +0000

    final

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let generateDomain = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounInsex = Math.floor(Math.random() * noun.length);
+  for (let i = 0; i < pronoun.length; i++)
+    for (let y = 0; y < adj.length; y++)
+      for (let x = 0; x < noun.length; x++)
+        console.log(pronoun[i] + adj[y] + noun[x] + ".com");
 };
+
+generateDomain();
